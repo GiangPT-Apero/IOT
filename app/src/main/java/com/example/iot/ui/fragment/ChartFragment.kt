669519dd329b7b3c1c0625a489baa701ce7edc11
zangizntPT <giangPT@apero.vn>
2024-08-27 08:31:00 +0700

    ui 90%

--- app/src/main/java/com/example/iot/ui/fragment/ChartFragment.kt
+++ app/src/main/java/com/example/iot/ui/fragment/ChartFragment.kt
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.iot.R
 import com.example.iot.adapter.TableAdapter
 import com.example.iot.databinding.FragmentChartBinding
+import com.example.iot.databinding.TopBarDeviceTableBinding
+import com.example.iot.databinding.TopBarSensorTableBinding
 import com.example.iot.ui.base.BaseFragment
 import com.example.iot.viewmodel.DeviceViewModel
 import com.example.iot.viewmodel.SensorViewModel
@@ -60,11 +62,23 @@ class ChartFragment : BaseFragment<FragmentChartBinding>(R.layout.fragment_chart
             binding.txtSensor.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.dark_464646)
             binding.txtDevice.setTextColor(ContextCompat.getColorStateList(requireContext(), R.color.dark_464646))
             binding.txtDevice.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.white)
+
+            val sensorTopBar = TopBarSensorTableBinding.inflate(layoutInflater, binding.llTopBar, false)
+            binding.llTopBar.apply {
+                removeAllViews()
+                addView(sensorTopBar.root)
+            }
         } else {
             binding.txtDevice.setTextColor(ContextCompat.getColorStateList(requireContext(), R.color.white))
             binding.txtDevice.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.dark_464646)
             binding.txtSensor.setTextColor(ContextCompat.getColorStateList(requireContext(), R.color.dark_464646))
             binding.txtSensor.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.white)
+
+            val deviceTopBar = TopBarDeviceTableBinding.inflate(layoutInflater, binding.llTopBar, false)
+            binding.llTopBar.apply {
+                removeAllViews()
+                addView(deviceTopBar.root)
+            }
         }
     }
 }
